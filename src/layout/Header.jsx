92766ab1e1f979b75b9f80af3551658f1fff0be4
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-07-15 14:52:48 +0900

    레이아웃 프레임

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -1,8 +1,17 @@
+import { HeaderWrapper, StyledHeader, ButtonsWrapper } from "./style";
 
 function Header() {
   return (
-    <div>Header</div>
-  )
+    <StyledHeader>
+      <HeaderWrapper>
+        <div className="logo">Moview</div>
+        <ButtonsWrapper>
+          <button>로그인</button>
+          <button>회원가입</button>
+        </ButtonsWrapper>
+      </HeaderWrapper>
+    </StyledHeader>
+  );
 }
 
-export default Header
+export default Header;
--- src/layout/style.js
+++ src/layout/style.js
@@ -3,4 +3,32 @@ import { styled } from "styled-components";
 export const StyledLayout = styled.main`
   margin: 0px auto;
   max-width: 1320px;
-`
+`;
+
+export const StyledHeader = styled.header`
+  background: beige;
+  height: 40px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const HeaderWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 1320px;
+`;
+
+export const ButtonsWrapper = styled.div`
+  display: flex;
+  gap: 30px;
+`;
+
+export const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+
+  & div {
+    width: 1320px;
+  }
+`;
