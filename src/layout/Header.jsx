e51a6575b06944e93f4563c5e62b27d0f7d17368
jwc406 <77850103+jwc406@users.noreply.github.com>
2023-07-20 01:22:24 +0900

    헤더 조건부 완료, 리뷰페이지 헤더만 스타일 고정, 자잘한 ui 수정

--- src/layout/Header.jsx
+++ src/layout/Header.jsx
@@ -5,7 +5,7 @@ import { ReactComponent as WhiteLogo } from "../assets/icons/logoWhite.svg";
 import { ReactComponent as BlackLogo } from "../assets/icons/logoBlack.svg";
 
 import { StyleSheetManager } from "styled-components";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import useModal from "../hooks/useModal";
 import { HeaderButton } from "./layoutStyle";
 import LoginModal from "../components/modals/LoginModal";
@@ -16,8 +16,11 @@ import { logout } from "../api/user";
 import { clearUserToken } from "../redux/modules/userTokenSlice";
 
 import { useSelector } from "react-redux";
+import { ReactComponent as User } from "../assets/icons/user.svg";
 
 function Header() {
+  const location = useLocation();
+
   const navigate = useNavigate();
   const [isScrolled, setIsScrolled] = useState(false);
   const headerRef = useRef(null);
@@ -38,10 +41,12 @@ function Header() {
   // Props 에러 방지
   const shouldForwardProp = (prop) => prop !== "isScrolled";
 
-  const [Modal, openModal, closeModal, openerRef] = useModal();
+  const [Modal, openModal, closeModal, openerRef] = useModal(); // 로그인 모달
+  // 회원가입 모달
   const [SigninModal, openSigninModal, closeSigninModal, openerSigninModalRef] =
     useModal();
 
+  // 토큰 삭제를 위한 디스패치
   const dispatch = useDispatch();
 
   const handleLogout = async () => {
@@ -52,50 +57,125 @@ function Header() {
       alert("로그아웃되었습니다.");
     } catch (error) {
       console.error("로그아웃 에러:", error);
-      // 에러 핸들링
       alert("로그아웃에 실패했습니다. 다시 시도해주세요.");
     }
   };
 
+  // 로그인 되었다면 토큰 여부 반환
   const isLogin = useSelector((state) => {
     return state.userToken.hasToken;
   });
 
   return (
     <StyleSheetManager shouldForwardProp={shouldForwardProp}>
-      <StyledHeader ref={headerRef} isScrolled={isScrolled}>
-        <HeaderWrapper>
-          <LogoBox onClick={() => navigate("/")}>
-            {isScrolled ? <BlackLogo /> : <WhiteLogo />}
-          </LogoBox>
-          <HeaderNav>
-            {isLogin ? (
-              <HeaderButton onClick={handleLogout}>로그아웃</HeaderButton>
-            ) : (
-              <HeaderButton onClick={openModal} ref={openerRef} color="white">
-                로그인
-              </HeaderButton>
-            )}
-            {Modal && (
-              <Modal>
-                <LoginModal closeLogin={closeModal} />
-              </Modal>
-            )}
-            <HeaderButton
-              onClick={openSigninModal}
-              ref={openerSigninModalRef}
-              isScrolled={isScrolled}
-            >
-              회원가입
-            </HeaderButton>
-            {SigninModal && (
-              <SigninModal>
-                <EmailSigninModal closeSignin={closeSigninModal} />
-              </SigninModal>
-            )}
-          </HeaderNav>
-        </HeaderWrapper>
-      </StyledHeader>
+      {/* 리뷰 페이지에서만 헤더 다르게 */}
+      {location.pathname.includes("/reviews/") ? (
+        <StyledHeader
+          ref={headerRef}
+          isScrolled={isScrolled}
+          style={{
+            background: "var(--white-Color)",
+            borderBottom: "1px solid var(--border-Color)",
+          }}
+        >
+          <HeaderWrapper>
+            <LogoBox onClick={() => navigate("/")}>
+              <BlackLogo />
+            </LogoBox>
+            <HeaderNav>
+              {/* 로그인 여부 따라 로그아웃 - 로그인 */}
+              {isLogin ? (
+                <HeaderButton
+                  onClick={handleLogout}
+                  style={{ color: "var(--font-Color)" }}
+                >
+                  로그아웃
+                </HeaderButton>
+              ) : (
+                <HeaderButton
+                  onClick={openModal}
+                  ref={openerRef}
+                  style={{ color: "var(--font-Color)" }}
+                >
+                  로그인
+                </HeaderButton>
+              )}
+              {Modal && (
+                <Modal>
+                  <LoginModal closeLogin={closeModal} />
+                </Modal>
+              )}
+              {isLogin ? (
+                <User fill="gray" />
+              ) : (
+                <HeaderButton
+                  onClick={openSigninModal}
+                  ref={openerSigninModalRef}
+                >
+                  회원가입
+                </HeaderButton>
+              )}
+              {SigninModal && (
+                <SigninModal>
+                  <EmailSigninModal closeSignin={closeSigninModal} />
+                </SigninModal>
+              )}
+            </HeaderNav>
+          </HeaderWrapper>
+        </StyledHeader>
+      ) : (
+        <StyledHeader ref={headerRef} isScrolled={isScrolled}>
+          <HeaderWrapper>
+            <LogoBox onClick={() => navigate("/")}>
+              {isScrolled ? <BlackLogo /> : <WhiteLogo />}
+            </LogoBox>
+            <HeaderNav>
+              {/* 로그인 여부 따라 로그아웃 - 로그인 */}
+              {isLogin ? (
+                <HeaderButton
+                  onClick={handleLogout}
+                  isScrolled={isScrolled}
+                  font
+                >
+                  로그아웃
+                </HeaderButton>
+              ) : (
+                <HeaderButton
+                  onClick={openModal}
+                  ref={openerRef}
+                  isScrolled={isScrolled}
+                  font
+                >
+                  로그인
+                </HeaderButton>
+              )}
+              {Modal && (
+                <Modal>
+                  <LoginModal closeLogin={closeModal} />
+                </Modal>
+              )}
+              {isLogin ? (
+                <User fill="gray" />
+              ) : (
+                <HeaderButton
+                  onClick={openSigninModal}
+                  ref={openerSigninModalRef}
+                  isScrolled={isScrolled}
+                  bgColor
+                  border
+                >
+                  회원가입
+                </HeaderButton>
+              )}
+              {SigninModal && (
+                <SigninModal>
+                  <EmailSigninModal closeSignin={closeSigninModal} />
+                </SigninModal>
+              )}
+            </HeaderNav>
+          </HeaderWrapper>
+        </StyledHeader>
+      )}
     </StyleSheetManager>
   );
 }
