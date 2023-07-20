95ff7fdee4acce4eac18efb78ccd5f73ea5ecce5
Yongjin An <yongjin5352@gmail.com>
2023-07-20 16:31:06 +0900

    프록시 제거

--- src/api/ourAxios.js
+++ src/api/ourAxios.js
@@ -1,13 +1,7 @@
 import axios from "axios";
 
-const host = (window.location.hostname === "localhost"
-  ? process.env.REACT_APP_SERVER_URL
-  : "/proxy"
-);
-
-
 const ourAxios = axios.create({
-  baseURL: host,
+  baseURL: process.env.REACT_APP_SERVER_URL,
 })
 
 export default ourAxios;
